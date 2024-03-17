c9f0e6c35f525aa4c3ed06cd71c161d994248af2
Karan Kumar <illuminatekaran@gmail.com>
2024-03-17 12:08:30 +0530

    mutex

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import mutex "github.com/karankumarshreds/concurrency"
+
+func main() {
+    mutex.ExampleA()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/karankumarshreds/concurrency
+
+go 1.22.0
