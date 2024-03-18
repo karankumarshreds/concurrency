d72d0ebb54e20f528977e8d36ef4afdcd92892df
Karan Kumar <illuminatekaran@gmail.com>
2024-03-19 00:55:37 +0530

    livelock

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-    "github.com/karankumarshreds/concurrency/mutex"
-    "github.com/karankumarshreds/concurrency/deadlock"
+	"fmt"
+	"time"
+
+	_ "github.com/karankumarshreds/concurrency/deadlock"
+	"github.com/karankumarshreds/concurrency/mutex"
 )
 
 func main() {
     mutex.Main()
-    deadlock.Main()
+    // deadlock.Main() this code will not run (on purpose)
+    for range time.Tick(1 *time.Second) {
+        fmt.Println("tick")
+    }
 }
